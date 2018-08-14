14d9587eb058370938882d883512925b3b752add
Lviv-code <palchinskiyv@gmail.com>
2018-08-14 12:32:49 +0300

    make pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,31 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/pages/main/main.component';
+import { MenuComponent } from './pages/pages/menu/menu.component';
+import { FooterComponent } from './pages/pages/footer/footer.component';
+import { NewsComponent } from './pages/pages/news/news.component';
+import { ContactsComponent } from './pages/pages/contacts/contacts.component';
+import { RoomsComponent } from './pages/pages/rooms/rooms.component';
+import { SliderComponent } from './pages/pages/main/slider/slider.component';
+import { ArticleComponent } from './pages/pages/article/article.component';
+import { RoomComponent } from './pages/pages/main/rooms/room/room.component';
+import { RoomDescriptionComponent } from './pages/pages/main/rooms/room-description/room-description.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    MenuComponent,
+    FooterComponent,
+    NewsComponent,
+    ContactsComponent,
+    RoomsComponent,
+    SliderComponent,
+    ArticleComponent,
+    RoomComponent,
+    RoomDescriptionComponent
   ],
   imports: [
     BrowserModule
