fd1c6f5c225d8c2e6ca0c82df626044e7566e6e8
xadriy <xandriy58@gmail.com>
2018-08-19 18:07:44 +0300

    added service for user service

---
+++ src/app/shared/service/user.service.ts
@@ -0,0 +1,44 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs/Observable";
+import {User} from "../models/user";
+
+@Injectable()
+export class UserService {
+
+  controller = "/user";
+
+  constructor(private _httpClient: HttpClient){
+
+  }
+
+  findAll(): Observable<User[]>{
+    return this._httpClient.get<User[]>(this.controller + '/find-all').catch(err => Observable.throw(err));
+  }
+
+  findAllAvailable(): Observable<User[]>{
+    return this._httpClient.get<User[]>(this.controller + '/find-all-available').catch(err => Observable.throw(err));
+  }
+
+  findOne(id: number): Observable<User>{
+    return this._httpClient.get<User>(this.controller + '/find-one/' + id).catch(err => Observable.throw(err));
+  }
+
+  findOneAvailable(id: number): Observable<User>{
+    return this._httpClient.get<User>(this.controller + '/find-one-available/' + id).catch(err => Observable.throw(err));
+  }
+
+  save(user: User): Observable<User>{
+      return this._httpClient.post<User>(this.controller + '/save', JSON.stringify(user)).catch(err => Observable.throw(err));
+  }
+
+  update(user: User): Observable<User>{
+    return this._httpClient.post<User>(this.controller + '/update',JSON.stringify(user)).catch(err => Observable.throw(err));
+  }
+
+  delete(id: number): Observable<any>{
+    return this._httpClient.delete(this.controller + '/delete/' + id).catch(err => Observable.throw(err));
+  }
+
+
+}
