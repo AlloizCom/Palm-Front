8d4f65ed12611c2f038d7ec0a75e24a8e14ad6c6
nazar.yurkiw <yurkiw.nazar@gmail.com>
2018-09-07 15:42:29 +0300

    some validators and tooltips

--- src/app/pages/cabinet/update/tariff/tariff-one/tariff-one.component.ts
+++ src/app/pages/cabinet/update/tariff/tariff-one/tariff-one.component.ts
@@ -1,35 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import {Tariff} from "../../../../../shared/models/tariff";
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {TariffService} from "../../../../../shared/service/tariff.service";
+import {FormControl, FormGroup,Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-tariff-one',
   templateUrl: './tariff-one.component.html',
   styleUrls: ['./tariff-one.component.css'],
-  providers:[TariffService]
+  providers: [TariffService]
 })
 export class TariffOneComponent implements OnInit {
 
-  tariff: Tariff = new Tariff();
-  constructor(private _router: ActivatedRoute,private _tariffService: TariffService) {
-    _router.params.subscribe(next=>{
-      _tariffService.findOne(next['id']).subscribe(next=>{
-        this.tariff=next;
-      })
-    })
+  tariffForm: FormGroup;
+  id: number = 0;
+
+  constructor(private _router: ActivatedRoute, private _tariffService: TariffService) {
+    _router.params.subscribe(next => {
+      this.id = next['id'];
+    });
   }
 
 
   ngOnInit() {
+    this.tariffForm = new FormGroup({
+      id: new FormControl(),
+      available: new FormControl(),
+      tariffType: new FormControl('',[Validators.required,Validators.min(1)]),
+      price: new FormControl('',[Validators.required]),
+      dateFrom: new FormControl('',[Validators.required]),
+      dateTo: new FormControl('',[Validators.required]),
+      roomType: new FormControl('',[Validators.required])
+    });
+    this._tariffService.findOne(this.id).subscribe(next=>{
+      console.log(next);
+      this.tariffForm.patchValue(<any>next);
+    },err=>{
+      console.error(err);
+    });
   }
 
-  update(form:HTMLFormElement){
-    console.log(this.tariff);
-    this._tariffService.update(this.tariff).subscribe(next=>{
-      this.tariff=next;
-    },error=>{
-      console.log(error);
+  update() {
+    console.log(this.tariffForm.getRawValue());
+    this._tariffService.update(this.tariffForm.getRawValue()).subscribe(next => {
+      this.tariffForm.patchValue(<any>next);
+    }, error => {
+      console.error(error);
     })
   }
 
