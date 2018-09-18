ae3d16a98712caf38cd84da488f0089b464ff5e4
Andriy Khalus <xandriy58@gmail.com>
2018-09-18 16:44:49 +0300

    added proposal compoment to add

--- src/app/pages/cabinet/add/add.module.ts
+++ src/app/pages/cabinet/add/add.module.ts
@@ -7,12 +7,13 @@ import {TariffComponent} from './tariff/tariff.component';
 import {GlobalImportsModule} from '../../../shared/config/global-imports/global-imports.module';
 import {AmenityNameComponent} from './amenity-name/amenity-name.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { ProposalComponent } from './proposal/proposal.component';
 
 @NgModule({
   imports: [
     GlobalImportsModule
   ],
-  declarations: [RoomComponent, NewsComponent, OptionComponent, CallbackComponent, TariffComponent, AmenityNameComponent, MainPageComponent],
+  declarations: [RoomComponent, NewsComponent, OptionComponent, CallbackComponent, TariffComponent, AmenityNameComponent, MainPageComponent, ProposalComponent],
 })
 export class AddModule {
 }
