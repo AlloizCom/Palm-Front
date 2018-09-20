80a8bc1232c36f4f114ec78b0f4b4c41b4ed2a56
xadriy <xandriy58@gmail.com>
2018-09-21 01:22:56 +0300

    added CallbackCounterService and add to cabinet component

--- src/app/pages/cabinet/cabinet.component.ts
+++ src/app/pages/cabinet/cabinet.component.ts
@@ -3,25 +3,34 @@ import {TranslateService} from "ng2-translate";
 import * as Stomp from 'stompjs';
 import * as SockJS from 'sockjs-client';
 import {NotificationService} from "../../../shared/service/notification.service";
+import {CallbackCounter} from "../../../shared/models/callback-counter";
+import {CallbackCounterService} from "../../../shared/service/callback-counter.service";
 @Component({
   selector: 'app-cabinet',
   templateUrl: './cabinet.component.html',
   styleUrls: ['./cabinet.component.css'],
+  providers: [CallbackCounterService]
 })
 export class CabinetComponent implements OnInit {
   isIn = true;
-  notifications : number ;
+  bookingNotifications : number ;
+  callbackNotifications : number ;
 
   private serverUrl = 'http://alloiz.hopto.org:8080/socket';
   private  stompClient;
 
   constructor(private _translate:TranslateService,
-              private _notificationService: NotificationService) {
+              private _notificationService: NotificationService,
+              private _callBackCounterService: CallbackCounterService) {
     this._translate.use('uk');
     this.initializeWebSocketConnection();
     this._notificationService.getCount().subscribe(next=>{
-      this.notifications = next.numberOfBooking;
-    })
+      this.bookingNotifications = next.numberOfBooking;
+    });
+    this._callBackCounterService.getCount().subscribe(next=>{
+      this.callbackNotifications = next.numberOfCallback;
+    });
+
   }
 
 
@@ -34,7 +43,15 @@ export class CabinetComponent implements OnInit {
         console.log(message.body);
         if(message.body) {
           console.log(JSON.parse(message.body).numberOfBooking);
-          this.notifications = JSON.parse(message.body).numberOfBooking;
+          this.bookingNotifications = JSON.parse(message.body).numberOfBooking;
+          console.log(message.body);
+        }
+      });
+      that.stompClient.subscribe("/callback/not", (message) => {
+        console.log(message.body);
+        if(message.body) {
+          console.log(JSON.parse(message.body).numberOfCallbacks);
+          this.callbackNotifications = JSON.parse(message.body).numberOfCallbacks;
           console.log(message.body);
         }
       });
---
+++ src/shared/service/callback-counter.service.ts
@@ -0,0 +1,31 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs/Observable";
+import {CallbackCounter} from "../models/callback-counter";
+
+@Injectable()
+export class CallbackCounterService {
+
+  controller = "/callback-gcounter";
+
+  constructor(private _httpClient: HttpClient){
+
+  }
+
+  getCount(): Observable<CallbackCounter>{
+    return this._httpClient.get<CallbackCounter>('/notify-callback')
+      .catch(err => Observable.throw(err));
+  }
+
+  resetCounter(): Observable<CallbackCounter>{
+    return this._httpClient.get<CallbackCounter>(this.controller + '/reset-callback')
+      .catch(err => Observable.throw(err));
+  }
+
+  incrementCounter(): Observable<CallbackCounter>{
+    return this._httpClient.get<CallbackCounter>(this.controller + '/reset-callback')
+      .catch(err => Observable.throw(err));
+  }
+
+
+}
