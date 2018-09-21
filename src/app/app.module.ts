7353c27b08c620c3ac2d11bcffdb731a8a3547ce
DanyloZasymenko <danik1492@gmail.com>
2018-09-21 13:31:01 +0300

    some fix in path by Danylo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {AppComponent} from './app.component';
-import {PagesModule} from './pages/pages/pages.module';
-import {RouterModule} from '@angular/router';
-import {routes} from './app.routes';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {CabinetModule} from './pages/cabinet/cabinet.module';
-import {CallbackService} from '/shared/service/callback.service';
-import {UserDetailsService} from '/shared/service/user-details.service';
-import {MyInterceptor} from '/shared/interceptors/my.interceptor';
-import {HTTP_INTERCEPTORS} from '@angular/common/http';
-import {GlobalImportsModule} from '/shared/config/global-imports/global-imports.module';
-import {PipeModule} from '/shared/pipe/pipe.module';
-import {NewsService} from '/shared/service/news.service';
-import {ServiceService} from "/shared/service/service.service";
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from "@angular/core";
+import {AppComponent} from "./app.component";
+import {BrowserModule} from "@angular/platform-browser";
+import {PagesModule} from "./pages/pages/pages.module";
+import {CabinetModule} from "./pages/cabinet/cabinet.module";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {BsDatepickerModule} from "ngx-bootstrap";
+import {FormsModule} from "@angular/forms";
+import {GlobalImportsModule} from "../shared/config/global-imports/global-imports.module";
+import {DateValueAccessorModule} from "angular-date-value-accessor";
+import {routes} from "./app.routes";
+import {PipeModule} from "../shared/pipe/pipe.module";
+import {RouterModule} from "@angular/router";
+import {CallbackService} from "../shared/service/callback.service";
+import {ServiceService} from "../shared/service/service.service";
+import {NewsService} from "../shared/service/news.service";
 import {MenuComponent} from "./pages/pages/menu/menu.component";
-import {DateValueAccessorModule} from 'angular-date-value-accessor';
-import {BsDatepickerModule} from 'ngx-bootstrap';
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {MyInterceptor} from "../shared/interceptors/my.interceptor";
+import {UserDetailsService} from "../shared/service/user-details.service";
 
 @NgModule({
   declarations: [
