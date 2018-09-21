8cb6e78ba7dbd0bc6a2b8217461cc2aedee06820
unknown <bohdanrud5180@gmail.com>
2018-09-21 12:31:32 +0300

    added model for Callback for pageable output

---
+++ src/shared/models/callback-by-page.ts
@@ -0,0 +1,10 @@
+import {Callback} from "./callback";
+
+export class CallbackByPage {
+
+  callback: Callback [] = [];
+  currentPage: number;
+  numberOfPages:number;
+  numberOfItems: number;
+
+}
