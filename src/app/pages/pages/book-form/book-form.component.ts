5f18cc33616d915657caa8f1255ca39e1c0438c0
Andriy Khalus <xandriy58@gmail.com>
2018-09-27 15:53:22 +0300

    added save date from booking and field message

--- src/app/pages/pages/book-form/book-form.component.ts
+++ src/app/pages/pages/book-form/book-form.component.ts
@@ -16,6 +16,7 @@ export class BookFormComponent implements OnInit {
   book: Book = new Book();
   roomParams: RoomsParams;
   roomTariff: any;
+  liqPayFormHtml: string = "";
 
   constructor(
     private _roomParamsService: RoomParamsService,
@@ -30,7 +31,8 @@ export class BookFormComponent implements OnInit {
       firstName: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]),
       lastName: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]),
       email: new FormControl('', [Validators.required, Validators.email]),
-      phoneNumber: new FormControl('', [Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')])
+      phoneNumber: new FormControl('', [Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')]),
+      message: new FormControl('')
     });
     this.bookForm.valueChanges.subscribe(value => {
       this.book = value;
@@ -46,5 +48,30 @@ export class BookFormComponent implements OnInit {
     //   });
     //   console.log(this.book);
     // }
+  }
+
+  pay() {
+
+    let book = new Book();
+    book.kids = this.roomParams.childrens;
+    book.dateIn = this.roomParams.dateFrom;
+    book.dateOut = this.roomParams.dateTo;
+    book.adults = this.roomParams.adults;
+    book.amountOfRooms = this.roomParams.numbersOfRooms;
+    book.roomType = this.roomParams.roomType;
+    book.email = this.bookForm.controls['email'].value;
+    book.firstName = this.bookForm.controls['firstName'].value;
+    book.lastName = this.bookForm.controls['lastName'].value;
+    book.phoneNumber = this.bookForm.controls['phoneNumber'].value;
+    book.message = this.bookForm.controls['message'].value;
+    console.log(this.bookForm);
+    console.log();
+
+    // this._bookService.pay(book).subscribe(next => {
+    //   this.liqPayFormHtml = next;
+    // });
+    this._bookService.pay(book).subscribe(next => {
+      this.liqPayFormHtml = next;
+    });;
   }
 }
