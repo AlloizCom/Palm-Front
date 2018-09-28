181c0dbd37510b1a6b7ae161cb23db0180fcea74
unknown <bohdanrud5180@gmail.com>
2018-09-28 11:33:11 +0300

    added new pipe for book status

---
+++ src/shared/pipe/pipe/book-status-pipe.pipe.ts
@@ -0,0 +1,20 @@
+import {Pipe, PipeTransform} from "@angular/core";
+
+@Pipe({
+  name: 'bookStatusPipe'
+})
+
+export class BookStatusPipePipe implements PipeTransform {
+
+  transform(value: any, ...args: any[]): any {
+    if(value == 'AVAILABLE')
+      return 'ДОСТУПНИЙ';
+    else if(value == 'CANCELED')
+      return 'ВІДМІНЕНИЙ';
+    else if(value == 'PAID_BY_CARD')
+      return 'ОПЛАТА ЗДІЙСНЕНА';
+    else if(value == 'HAVE_TO_BE_PAID')
+      return 'ОПЛАТА ПРИ ЗАСЕЛЕННІ';
+  }
+
+}
