0d59ea85ba7017b6cbc55d0a7bfdfd35a30dded6
vetalolon <vetalolon@gmail.com>
2018-10-02 17:35:32 +0300

    photo

--- src/shared/config/url.ts
+++ src/shared/config/url.ts
@@ -1,3 +1,5 @@
 export const url = "http://alloiz.hopto.org:8080";
 export const urlFront = "http://alloiz.hopto.org:4200";
-// export const url = "http://alloiz.hopto.org:8080/test";
+// export const url = "http://a
+//
+// lloiz.hopto.org:8080/test";
