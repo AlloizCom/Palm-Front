b2c60fc77a3c11c26899ff13b5ba789865f15556
BillyTheWalker <bilevych85@gmail.com>
2018-10-13 11:39:52 +0300

    Update url.ts

--- src/shared/config/url.ts
+++ src/shared/config/url.ts
@@ -1,5 +1,7 @@
-export const url = "http://alloiz.hopto.org:8080";
-export const urlFront = "http://alloiz.hopto.org:4200";
+// export const url = "http://alloiz.hopto.org:8080";
+// export const urlFront = "http://alloiz.hopto.org:4200";
 // export const url = "http://a
 //
 // lloiz.hopto.org:8080/test";
+export const url = "http://185.233.116.57:8080";
+export const urlFront = "http://185.233.116.57";
