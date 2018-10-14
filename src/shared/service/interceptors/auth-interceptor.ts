bfde70481405393ef1ab49d6b4df3f5bea8f4645
billythewalker <bilevych85@gmail.com>
2018-10-14 16:43:18 +0300

    some

--- src/shared/service/interceptors/auth-interceptor.ts
+++ src/shared/service/interceptors/auth-interceptor.ts
@@ -1,11 +1,9 @@
-import {Injectable} from "@angular/core";
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import {Observable} from "rxjs/Observable";
-import {Router} from "@angular/router";
-import {isNullOrUndefined} from "util";
-import "rxjs/add/operator/catch";
-import "rxjs/add/observable/throw";
-import {UserDetailsService} from "../user-details.service";
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {Router} from '@angular/router';
+import {isNullOrUndefined} from 'util';
+import {UserDetailsService} from '../user-details.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -14,20 +12,21 @@ export class AuthInterceptor implements HttpInterceptor {
   }
 
   intercept<T>(req: HttpRequest<T>, next: HttpHandler): Observable<HttpEvent<T>> {
-    return next.handle(req).catch(err => {
+    let ret = next.handle(req);
+    ret.subscribe(value => {
+    }, err => {
       console.log(err.status);
-      if(err.status===404){
+      if (err.status === 404) {
         this._router.navigateByUrl('/error/404');
       }
       if (err.status === 401) {
         this._userDetailsService.logout();
         this._router.navigateByUrl('/login');
-        if (isNullOrUndefined(sessionStorage.getItem("refresh_token"))||isNullOrUndefined(localStorage.getItem("refresh_token"))) {
-          return Observable.throw(err);
+        if (isNullOrUndefined(sessionStorage.getItem('refresh_token')) || isNullOrUndefined(localStorage.getItem('refresh_token'))) {
         }
       } else {
-        return Observable.throw(err);
       }
     });
+    return ret;
   }
 }
