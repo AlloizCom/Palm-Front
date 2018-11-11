082a563ab1fa91a0954357594fb6d5ce02b7ff24
Volodymyr Bilevych <bilevych85@gmail.com>
2018-11-11 18:25:51 +0200

    fex

---
+++ src/app/pages/cabinet/update/user/user.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit} from '@angular/core';
+import {UserService} from '../../../../../shared/service/user.service';
+import {UserDetailsService} from '../../../../../shared/service/user-details.service';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  password: string = '';
+
+  constructor(private _userService: UserService, private _userDetails: UserDetailsService) {
+
+  }
+
+  update() {
+    if (this.password.length < 6) {
+      alert('Password must be at least 6 symbols length!');
+      return;
+    } else if (!this.password.match(/^(\w*\d*)+$/)) {
+      alert('Password must contain latin symbols and numbers and nothing else!');
+      return;
+    }
+    this._userDetails.user.password = this.password;
+    this._userService.update(this._userDetails.user)
+      .subscribe(arg => this._userDetails.updateUser(arg));
+
+  }
+
+  ngOnInit() {
+  }
+
+}
