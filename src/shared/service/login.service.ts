d918df05fa4830bba67b020f6a126e25db13c986
Volodymyr Bilevych <bilevych85@gmail.com>
2018-11-15 14:47:11 +0200

    some fixes

--- src/shared/service/login.service.ts
+++ src/shared/service/login.service.ts
@@ -3,7 +3,7 @@ import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
-import {User} from "../models/user";
+import {User} from '../models/user';
 
 // @Injectable()
 // export class LoginService {
@@ -23,20 +23,24 @@ import {User} from "../models/user";
 //       })
 //       .catch(error => Observable.throw(error));
 //   }
-  @Injectable()
-  export class LoginService {
+@Injectable()
+export class LoginService {
 
-  static tokenName: string = "access_token";
+  static tokenName: string = 'access_token';
 
   constructor(private _http: HttpClient) {
   }
 
-    sendCredentials(model:User): Observable<any> {
-    return this._http.post("/oauth/token", null, {
-      params: new HttpParams()
-        .set("username", model.login)
-        .set("password", model.password)
-        .set("grant_type", "password")
-    }).catch((error) => Observable.throw(error));
+  sendCredentials(model: User): Observable<any> {
+    // return this._http.post('/oauth/token', {grant_type: 'password', username: model.login, password: model.password});
+    return this._http.post('/oauth/token', null, {
+      params: new HttpParams({
+        fromObject: {
+          grant_type: 'password',
+          username: model.login,
+          password: model.password
+        }
+      })
+    });
   }
 }
