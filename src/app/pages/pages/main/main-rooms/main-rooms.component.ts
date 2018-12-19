046d17170dc52b8d68fa12c12f04e863e201f0b5
bohdanrud <bohdanrud5180@gmail.com>
2018-12-19 19:52:41 +0200

    fixed room slider

--- src/app/pages/pages/main/main-rooms/main-rooms.component.ts
+++ src/app/pages/pages/main/main-rooms/main-rooms.component.ts
@@ -43,7 +43,7 @@ export class MainRoomsComponent implements OnInit {
   }
 
   set currentI(value: number) {
-    if (value >= this.rooms.length - 1) {
+    if (value >= this.rooms.length) {
       this._currentI = 1;
       return;
     } else if (value < 1) {
