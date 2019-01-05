b46a1d7f043545d196eb1edf957aa06e3109b23f
Volodymyr Bilevych <bilevych85@gmail.com>
2019-01-05 13:53:10 +0200

    add google analitics addon

--- src/app/pages/pages/pages.component.ts
+++ src/app/pages/pages/pages.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
 import {CurrentLanguageService} from '../../../shared/service/current-language.service';
+import {BrowserCheckService} from '../../shared/service/browser-check.service';
 
 @Component({
   selector: 'app-pages',
@@ -14,21 +15,23 @@ export class PagesComponent implements OnInit {
     private _translate: TranslateService,
     private _router: Router,
     private _activateRouteSnapshot: ActivatedRoute,
-    private _currentLanguageService: CurrentLanguageService
+    private _currentLanguageService: CurrentLanguageService,
+    private _isBrowserService: BrowserCheckService
   ) {
     if (_activateRouteSnapshot.snapshot.params.lang &&
       _translate.getLangs().includes(_activateRouteSnapshot.snapshot.params.lang)) {
       this._translate.use(_activateRouteSnapshot.snapshot.params.lang);
     } else {
-      this._router.navigateByUrl('pages/'+_currentLanguageService.currentLanguage);
+      this._router.navigateByUrl('pages/' + _currentLanguageService.currentLanguage);
     }
-    // this._router.events.subscribe(value => {
-    //   if (value instanceof NavigationEnd) {
-    //     if (this._activateRouteSnapshot.snapshot.params.lang != this._currentLanguageService.currentLanguage) {
-    //       this._router.navigateByUrl(`${_currentLanguageService.currentLanguage}${value.urlAfterRedirects.replace(this._activateRouteSnapshot.snapshot.params.lang, '')}`);
-    //     }
-    //   }
-    // });
+    this._router.events.subscribe(value => {
+      if (event instanceof NavigationEnd) {
+        if (_isBrowserService.isBrowser()) {
+          (<any>window).ga('set', 'page', event.urlAfterRedirects);
+          (<any>window).ga('send', 'pageview');
+        }
+      }
+    });
   }
 
   ngOnInit() {
